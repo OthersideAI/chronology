48ee83089ffbc1aaed2494d16b46b5a4c152b6e0
Bram Adams <bramadams@brams-mbp.lan>
2020-12-07 14:04:44 -0500

    first commit

---
+++ chronology/__init__.py
@@ -0,0 +1,178 @@
+import asyncio
+import openai
+import os
+from pathlib import Path
+from loguru import logger
+from dotenv import load_dotenv
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# oai
+
+async def _search(q, docs, engine="ada"):
+    logger.debug("""CONFIG:
+    Query: {0}
+    Docs: {1}
+    Engine: {2}
+    """.format(q, docs, engine))
+    response = openai.Engine(engine).search(
+        documents=docs,
+        query=q
+    )
+    logger.debug("GPT-3 Search Result: {0}".format(response))
+    return response
+
+
+async def _completion(prompt, engine="ada", max_tokens=64, temperature=0.7, top_p=1, stop=None, presence_penalty=0, frequency_penalty=0, echo=False):
+    logger.debug("""CONFIG:
+    Prompt: {0}
+    Temperature: {1}
+    Engine: {2}
+    Max Tokens: {3}
+    Top-P: {4}
+    Stop: {5}
+    Presence Penalty {6}
+    Frequency Penalty: {7}
+    Echo: {8}"""
+                 .format(repr(prompt), temperature, engine, max_tokens, top_p, stop, presence_penalty, frequency_penalty, echo))
+    response = openai.Completion.create(engine=engine,
+                                        prompt=prompt,
+                                        max_tokens=max_tokens,
+                                        temperature=temperature,
+                                        top_p=top_p,
+                                        presence_penalty=presence_penalty,
+                                        frequency_penalty=frequency_penalty,
+                                        echo=echo,
+                                        stop=stop)
+    logger.debug("GPT-3 Completion Result: {0}".format(response))
+    return response
+
+# Helpers
+
+
+def _max_search_doc(resp):
+    max_doc = max(resp.data, key=lambda x: x['score'])
+    return (max_doc, max_doc.document, max_doc.score)
+
+
+def _fetch_response(resp):
+    return resp.choices[0].text
+
+
+def _trimmed_fetch_response(resp):
+    return resp.choices[0].text.strip()
+
+
+def prepend_prompt(new_stuff, prompt):
+    '''
+    Add new content to the start of a string.
+    '''
+    return "{0}{1}".format(new_stuff, prompt)
+
+
+def append_prompt(new_stuff, prompt):
+    '''
+    Add new content to the end of a string.
+    '''
+    return "{1}{0}".format(new_stuff, prompt)
+
+
+def add_new_lines_end(prompt, count):
+    '''
+    Add N new lines to the end of a string.
+    '''
+    return "{0}{1}".format(prompt, "\n"*count)
+
+
+def add_new_lines_start(prompt, count):
+    '''
+    Add N new lines to the start of a string.
+    '''
+    return "{1}{0}".format(prompt, "\n"*count)
+
+
+def read_prompt(filename):
+    '''
+    Looks in prompts/ directory for a text file. Pass in file name only, not extension.
+
+    Example: prompts/hello-world.txt -> read_prompt('hello-world')
+    '''
+    return Path('../prompts/{0}.txt'.format(filename)).read_text()
+
+
+async def gather(*args):
+    '''
+    Run methods in parallel (they don't need to wait for each other to finish).
+
+    Requires method argumets to be async.
+
+    Example: await gather(fetch_max_search_doc(query_1, docs), fetch_max_search_doc(query_2, docs))
+    '''
+    return await asyncio.gather(*args)
+
+# Wrappers
+
+
+async def cleaned_completion(prompt, engine="ada", max_tokens=64, temperature=0.7, top_p=1, stop=None, presence_penalty=0, frequency_penalty=0, echo=False):
+    '''
+    Wrapper for OpenAI API completion. Returns whitespace trimmed result from GPT-3.
+    '''
+    resp = await _completion(prompt,
+                            engine=engine,
+                            max_tokens=max_tokens,
+                            temperature=temperature,
+                            top_p=top_p,
+                            presence_penalty=presence_penalty,
+                            frequency_penalty=frequency_penalty,
+                            echo=echo,
+                            stop=stop)
+    return _trimmed_fetch_response(resp)
+
+
+async def raw_completion(prompt, engine="ada", max_tokens=64, temperature=0.7, top_p=1, stop=None, presence_penalty=0, frequency_penalty=0, echo=False):
+    '''
+    Wrapper for OpenAI API completion. Returns raw result from GPT-3.
+    '''
+    resp = await _completion(prompt,
+                            engine=engine,
+                            max_tokens=max_tokens,
+                            temperature=temperature,
+                            top_p=top_p,
+                            presence_penalty=presence_penalty,
+                            frequency_penalty=frequency_penalty,
+                            echo=echo,
+                            stop=stop)
+    return _fetch_response(resp)
+
+
+async def fetch_max_search_doc(q, docs, engine="ada", min_score_cutoff=-1, full_doc=False):
+    '''
+    Fetch document value with max score. Wrapper for OpenAI API Search.
+
+    Optional:
+
+    min_score_cutoff = if maximum score is less than cutoff, None will be returned. Defaults to -1
+
+    full_doc = return whole response with max, but doesn't grab doc for you. Defaults to False
+    '''
+    resp = await _search(q, docs, engine=engine)
+    if not full_doc: 
+        if float(_max_search_doc(resp)[2]) > min_score_cutoff:
+            return docs[_max_search_doc(resp)[1]]
+        else:
+            return None
+    else:
+        if float(_max_search_doc(resp)[2]) > min_score_cutoff:
+            return _max_search_doc(resp)
+        else:
+            return None
+
+
+def main(fn):
+    '''
+    Main function that runs logic. Accepts a function implemented on your end!
+    '''
+    asyncio.run(fn())
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='chronology',
+    version='0.0.1',
+    description='Chain GPT calls like a pro!',
+    url='git@github.com:bramses/chronology-ai.git',
+    author='Otherside AI',
+    author_email='bram@othersideai.com',
+    license='unlicense',
+    packages=['chronology-ai'],
+    zip_safe=False
+)
