e2d88af606bef3fbbcfeff3b2873a9e65ce0332d
Bram Adams <bramadams@brams-mbp.lan>
2020-12-07 16:38:11 -0500

    url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='chronological',
     version='0.0.3',
     description='Chain GPT calls like a pro!',
-    url='git@github.com:bramses/chronology-ai.git',
+    url='https://github.com/bramses/chronology.git',
     author='Otherside AI',
     author_email='bram@othersideai.com',
     license='unlicense',
