8978465e7d6267bc315e8d96ff0b143c6d75da96
Bram Adams <3282661+bramses@users.noreply.github.com>
2020-12-30 13:54:11 -0500

    pkg built w Twine

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='chronological',
-    version='0.0.9',
+    version='0.1.0',
     description='Chain GPT calls like a pro!',
-    url='https://github.com/bramses/chronology.git',
+    url='https://github.com/OthersideAI/chronology',
     author='Otherside AI',
     author_email='bram@othersideai.com',
     license='unlicense',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['openai', 'python-dotenv'],
+    install_requires=['openai', 'python-dotenv', 'loguru'],
     zip_safe=False,
     python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
